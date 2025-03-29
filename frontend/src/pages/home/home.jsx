928b52acd39555bef773868a2ca89a3d24bf0320
Matthew Keshishian <matthewkesh8@gmail.com>
2025-03-28 19:52:07 -0700

    integrated Google Calendar

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
             <AuthRedirect/>
             <div class="container">
                 <div class="cal">
-                <iframe src="https://calendar.google.com/calendar/embed?src=aeobruin%40gmail.com&ctz=America%2FLos_Angeles" style={{border: 0}} width="800" height="600" frameborder="0" scrolling="no"></iframe>
+                  <iframe src="https://calendar.google.com/calendar/embed?src=aeobruin%40gmail.com&ctz=America%2FLos_Angeles" class="gcal" style={{border: 0}}  frameborder="0" scrolling="no"></iframe>
                 </div>
             </div>
 
